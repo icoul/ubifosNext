3b739a7db09269862d5fe97e653d0c8a3e05cf18
icoul <icoul35@gmail.com>
2022-09-01 16:10:50 +0900

    nextJS 설정 파일에서 불필요한 webpack 설정내용제거

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  future: {
-    webpack5: true,
-  },
-  webpack: function (config, options) {
-    config.experiments = {};
-    return config;
-  },
   reactStrictMode: true,
   swcMinify: true,
 }
