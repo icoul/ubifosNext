2604cc187fa1b059ca0d0a8093b7bcf90c09441a
icoul <icoul35@gmail.com>
2022-09-01 12:37:41 +0900

    nextJS 설정 파일에 webpack설정 추가

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  future: {
+    webpack5: true,
+  },
+  webpack: function (config, options) {
+    config.experiments = {};
+    return config;
+  },
   reactStrictMode: true,
   swcMinify: true,
 }
